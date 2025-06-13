8a9289a4679e0880c5c8e799fb6c98bc5eabbf8f
João Vítor mendes <88053372+Joaomendes21@users.noreply.github.com>
2025-06-13 08:07:42 -0300

    aulas de python sobre format e input.

---
+++ aula32_format.py
@@ -0,0 +1,6 @@
+a = 'a'
+b = 'a'
+c = 1.1
+formato = 'a={} b={} c={}'.format(a, b, c)
+print(formato)
+
---
+++ aula33_input.py
@@ -0,0 +1,9 @@
+# nome = input('qual é o seu nome? ')
+# print(f'O seu nome é {nome}')
+# print(f'A soma dos numeros são: {int(numero1) + int(numero2)}')
+numero1 = input('Digite um numero: ')
+numero2 = input('Digite outro numero: ')
+n1 = int(numero1)
+n2 = int(numero2)
+
+print(f'A soma dos numeros são: {n1 + n2}')
