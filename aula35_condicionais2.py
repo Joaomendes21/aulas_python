ab544d2f7a147ef03adc05a1e13d8ba06d126603
João Vítor mendes <88053372+Joaomendes21@users.noreply.github.com>
2025-06-14 09:16:53 -0300

    add condicionais

---
+++ aula35_condicionais2.py
@@ -0,0 +1,19 @@
+# if/ elif / else
+# se / se não se /  se não
+
+condiçao1 = False
+condiçao2 = False
+condiçao3 = True
+condiçao4 = False
+
+if condiçao1:
+    print('Este é o codigo condiçao1')
+elif condiçao2:
+    print('Este é o codigo condiçao2')
+elif condiçao3:
+    print('Este é o codigo condiçao3')
+elif condiçao4:
+    print('Este é o codigo condiçao4')
+else:
+    print('Nenhuma condição foi satisfeita.')
+
