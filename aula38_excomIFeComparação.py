c3b5e5aec86d9d901f9ffda0934d953957507dbc
João Vítor mendes <88053372+Joaomendes21@users.noreply.github.com>
2025-06-15 08:29:29 -0300

    add aulas de operadores e exercício com IF e comparação.

---
+++ aula38_excomIFeComparação.py
@@ -0,0 +1,11 @@
+valor1 = input('Digite um valor:')
+valor2 = input('Digite o segundo valor:')
+
+if valor1 > valor2:
+    print(f'O valor {valor1} é maior que {valor2}.')
+elif valor1 < valor2:
+    print(f'O valor {valor1} é menor que {valor2}')
+else:
+    print(f'O valor {valor1} e o valor {valor2} são iguais.')
+
+    
