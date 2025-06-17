033da2eb0e2701d7da4e5db8e573b2ae60b48e95
João Vítor mendes <88053372+Joaomendes21@users.noreply.github.com>
2025-06-17 07:12:46 -0300

    Add aula de interpolação

---
+++ aula44_interpolação.py
@@ -0,0 +1,11 @@
+'''
+Interpolção básica de strings
+s - strings
+d e i - int 
+f - float
+x e X - hexadecimal (ABCDF0123456789)
+'''
+nome = 'João'
+preco = 1000.95897643
+variavel = '%s, o preço é R$%.2f' %(nome, preco)
+print(variavel)
