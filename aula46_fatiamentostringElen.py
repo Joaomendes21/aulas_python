b727c02f9f71a612b44ecbf5b99148d03ae42d32
João Vítor mendes <88053372+Joaomendes21@users.noreply.github.com>
2025-06-18 08:01:56 -0300

    Add aula de fatiamento de strings e função len

---
+++ aula46_fatiamentostringElen.py
@@ -0,0 +1,12 @@
+'''
+Fatiamento de strings
+012345678
+Olá mundo
+-987654321
+Fatiamento [i:f:p][::]
+obs.: a função le retorna a qtd
+de caracteres da str
+'''
+
+variavel = 'Olá mundo'
+print(len(variavel))
