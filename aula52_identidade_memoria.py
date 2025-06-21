69bf57ef1a451ca5de6215d74aea86e3e0e3d760
João Vítor mendes <88053372+Joaomendes21@users.noreply.github.com>
2025-06-21 11:29:44 -0300

    Add aula de identidade e gravação na memoria

---
+++ aula52_identidade_memoria.py
@@ -0,0 +1,16 @@
+'''
+Flag (Bandeira) - Marcar um local
+None = Não valor
+is e is not = é ou não é (tipo, valor, identidade)
+id = identidade
+'''
+v1 = 'a'
+v2 = 'a'
+print(id(v1))
+print(id(v2))
+# condição = false
+
+# if condição:
+#     print('faça algo')
+# else:
+#     print('não faça algo')
